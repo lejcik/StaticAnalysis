7318ac3790f52a6b81c4a42446032d81b73b3109
Viliam Lejčík <34658965+lejcik@users.noreply.github.com>
2021-12-03 21:09:15 +0100

    added verbosity

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,13 +28,17 @@ cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON "$INPUT_CMAKE_ARGS" ..
 
 files_to_check=$(python3 /get_files_to_check.py -exclude="$INPUT_EXCLUDE_DIR" -json="compile_commands.json")
 
+# Excludes for clang-tidy are handled in python script
+echo "== Running clang analysis..."
+eval clang-tidy-12 "$INPUT_CLANG_TIDY_ARGS" -p "$(pwd)" "$files_to_check" -- >"clang_tidy.txt"
+echo "== Done."
+
+echo "== Running cppcheck analysis..."
 if [ -z "$INPUT_EXCLUDE_DIR" ]; then
     eval cppcheck --project=compile_commands.json "$INPUT_CPPCHECK_ARGS" --output-file=cppcheck.txt
 else
     eval cppcheck --project=compile_commands.json "$INPUT_CPPCHECK_ARGS" --output-file=cppcheck.txt -i"$GITHUB_WORKSPACE/$INPUT_EXCLUDE_DIR"
 fi
-
-# Excludes for clang-tidy are handled in python script
-eval clang-tidy-12 "$INPUT_CLANG_TIDY_ARGS" -p "$(pwd)" "$files_to_check" -- >"clang_tidy.txt"
+echo "== Done."
 
 python3 /run_static_analysis.py -cc cppcheck.txt -ct clang_tidy.txt -o $print_to_console
